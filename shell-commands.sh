1f9323951e1f02deb2c5527cef6bfa27b28cb405
Bobby ABUCHI <bobbyabuchi@gmail.com>
2024-06-03 11:13:00 +0100

    Update shell-commands.sh

--- shell-commands.sh
+++ shell-commands.sh
@@ -2,3 +2,9 @@
 
 ls -l # list content of a directory with more details
 ls -l Documents # show what's in docs with even more details
+ls -l Documents/*.pdf # show all PDF files in Docs dir
+
+mv 'Documents/notes.txt' Documents/MyTexts/ # move the notes.txt file from Documents to sub dir MyTexts
+mv 'Documents/*.txt' Documents/MyTexts/ # move all the .txt files from Documents to sub dir MyTexts
+
+n
